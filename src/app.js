c8610cd2129324b69bdd3ff479ade2926fdbfe8d
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-02-19 17:08:25 +0100

    connect netlify

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const serverless = require('serverless-http');
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('<h2>Welcome to Task App!</h2>')
+})
+app.use(userRouter)
+app.use(taskRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/.netlify/functions/app', app);
+    module.exports.handler = serverless(app);
+
+} else {
+    app.listen(process.env.PORT, () => {
+        console.log('Server is up on port ' + process.env.PORT)
+    })
+}
