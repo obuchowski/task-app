192641f76c164bdac0b428f1aaa52313ada5a67f
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-02-20 00:19:01 +0100

    remove sendgrid

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,23 +1,43 @@
-const sgMail = require('@sendgrid/mail')
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const nodemailer = require('nodemailer');
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    host: 'smtp.yandex.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.YA_U,
+        pass: process.env.YA_P
+    }
+})
 
 const sendWelcomeEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'evgeni@obukhovski.com',
-        subject: 'Thanks for joining in!',
-        text: `Welcome to the app, ${name}.`
-    })
+    try {
+        const mailOptions = {
+            from: '"Evgeni Obukhovski" <evgeni@obukhovski.com>',
+            to: email,
+            subject: 'Thanks for joining in!',
+            text: `Welcome to the app, ${name}.`,
+            html: `<b>Welcome to the app, ${name}.</b>`
+        }
+        transporter.sendMail(mailOptions)
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 const sendGoodbyeEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'evgeni@obukhovski.com',
-        subject: 'Chao!',
-        text: `Bye, ${name}.`
-    })
+    try {
+        const mailOptions = {
+            from: '"Evgeni Obukhovski" <evgeni@obukhovski.com>',
+            to: email,
+            subject: 'Chao!',
+            text: `Bye, ${name}.`,
+            html: `<b>Bye, ${name}.</b>`
+        }
+        transporter.sendMail(mailOptions)
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 module.exports = {
