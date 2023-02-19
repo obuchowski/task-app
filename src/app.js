36c219230e94991f27b73c48fb5a9e83c00116e8
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-02-19 21:34:03 +0100

    Initialize project using Create React App connect netlify: test deploy#6

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const serverless = require('serverless-http')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const mongoose = require("mongoose")
+const saslprep = require('saslprep')
+const mongoose = require('mongoose')
 mongoose.set('useNewUrlParser', true)
     .set('useUnifiedTopology', true)
     .set('strictQuery', false)
@@ -13,7 +14,7 @@ const app = express()
 const router = express.Router()
 
 app.use((req, res, next) => {
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(saslprep(process.env.MONGODB_URI))
     res.on('finish', () => {
         mongoose.connection.close()
     });
