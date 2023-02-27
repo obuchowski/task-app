a446ce1a612e7d9bef0ad4cacbe77f94c85259d0
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-02-27 01:36:32 +0100

    test firebase

---
+++ functions/firebase.js
@@ -0,0 +1,25 @@
+const process = require('process')
+const { Firestore } = require('@google-cloud/firestore')
+
+const handler = async function (event) {
+    const firestore = new Firestore({
+        projectId: process.env.GOOGLE_APPLICATION,
+        credentials: {
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }
+    })
+    const tasksRef = firestore.collection('tasks'),
+        snapshot = await tasksRef.get(),
+        response = {}
+    snapshot.forEach((doc) => {
+        response[doc.id] = doc.data()
+    })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
+}
+
+module.exports = { handler }
